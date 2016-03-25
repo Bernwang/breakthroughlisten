7df144f91fd8774b7f524e1ebc420bf8088c0428
Ankush Prakash <ankush.prakash17@berkeley.edu>
2016-03-25 14:42:48 -0700

    Added labels and changed colors

--- ankush/map.py
+++ ankush/map.py
@@ -1,9 +1,11 @@
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import csv
 
-star_type_dict = {'A':'c', 'B':'b', 'F':'r', 'K':'k', 'M':'m','G':'g'}
+# M and K are shades of red, G and F are shades of orange, A is yellow, B is a shade of white(grayish),'O' is blue
+star_type_dict = {'M':'#990000', 'K':'#ff0000', 'G':'#996300', 'F':'#ffa500', 'A':'#FFFF00','B':'#cccccc','O':'#0000FF'}
 def read_file(fileName,map_object):
 	with open(fileName, 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar="'")
@@ -20,6 +22,15 @@ def read_file(fileName,map_object):
 					star_type = line[5].strip('"') # Analyzes the type attribute
 					map_object.plot(x_coord, y_coord, marker = ".", color=star_type_dict[star_type[0]])
 			counter += 1
+	
+	M = mpatches.Patch(color='#990000', label='M')
+	K = mpatches.Patch(color='#ff0000', label='K')
+	G = mpatches.Patch(color='#996300', label='G')
+	F = mpatches.Patch(color='#ffa500', label='F')
+	A = mpatches.Patch(color='#FFFF00', label='A')
+	B = mpatches.Patch(color='#cccccc', label='B')
+	O = mpatches.Patch(color='#0000FF', label='O')
+	plt.legend(handles=[M,K,G,F,A,B,O],bbox_to_anchor=(.1, .2))
 	plt.show()
 
 def convert_to_degrees(s,hours = False):
@@ -37,8 +48,8 @@ def convert_to_degrees(s,hours = False):
 
 m = Basemap(projection='moll',lon_0=0,lat_0=0)
 parallels = np.arange(-90.,90,30.)
-m.drawparallels(parallels)
-m.drawmeridians(np.arange(0.,360.,30.))
+m.drawparallels(parallels,labels=[True,True,True,True,True])
+m.drawmeridians(np.arange(0.,360.,30.),labels=[True,False,False,False,False])
 m.drawmapboundary()
 read_file("sampled_data.csv",m)
 
