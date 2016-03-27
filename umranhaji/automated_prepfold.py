0ab77bb3df22ba92c119939eb7e77d4659154c82
umranhaji <Umran@Umrans-MacBook-Pro.local>
2016-03-26 22:26:56 -0700

    changes

--- umranhaji/automated_prepfold.py
+++ umranhaji/automated_prepfold.py
@@ -1,10 +1,6 @@
-#This script takes all the .fil files in the directory and uses the docker image caseyjlaw/pulsar-psr to run the prepfold command on them.
+#This script takes all the .fil files in the directory and uses the docker imag\
+e caseyjlaw/pulsar-psr to run the prepfold command on them.                     
 
 import subprocess
 subprocess.call('eval $(docker-machine env default)', shell=True)
-subprocess.call('docker images', shell=True)
-#subprocess.call('docker run caseyjlaw/pulsar-stack', shell=True)
-
-
-#subprocess.call('docker run caseyjlaw/pulsar-stack', shell=True)
-
+subprocess.call('docker run caseyjlaw/pulsar-stack')
